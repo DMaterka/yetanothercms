4a090b2d766a9f5402cc01b9714e7879c3d5e435
Daniel Materka <daniel.materka@gmail.com>
2020-03-08 21:56:57 +0100

    clean up

--- migrate.php
+++ migrate.php
@@ -4,33 +4,29 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = new \Symfony\Component\Dotenv\Dotenv();
 $dotenv->load(__DIR__.'/.env');
 
-try {
-    $connection =  \Database\DB::getConnection();
-    $connection->beginTransaction();
 
-    //create users table
-    $connection->query('
-        create table users(
-        id INT NOT NULL AUTO_INCREMENT,
-        email VARCHAR(40) NOT NULL,
-        password VARCHAR(100) NOT NULL,
-        PRIMARY KEY ( id )
-        );
-    ');
+$connection =  \Database\DB::getConnection();
+$connection->beginTransaction();
 
-    //create articles table
-    $connection->query('
-        create table articles(
-           id INT NOT NULL AUTO_INCREMENT,
-           title VARCHAR(40) NOT NULL,
-           intro VARCHAR(100) NOT NULL,
-           content VARCHAR(255) NOT NULL,
-           PRIMARY KEY ( id )
-        );
-    ');
+//create users table
+$connection->query('
+    create table users(
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(40) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    PRIMARY KEY ( id )
+    );
+');
 
-    $connection->commit();
+//create articles table
+$connection->query('
+    create table articles(
+       id INT NOT NULL AUTO_INCREMENT,
+       title VARCHAR(40) NOT NULL,
+       intro VARCHAR(100) NOT NULL,
+       content VARCHAR(255) NOT NULL,
+       PRIMARY KEY ( id )
+    );
+');
 
-} catch (PDOException $exception) {
-    echo  $exception->getMessage();
-}
+$connection->commit();
